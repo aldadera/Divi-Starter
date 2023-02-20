61508261d7a44559ce66ee3a9ca7d6dcefed954c
aldadera <aldaderaw@gmail.com>
2023-02-20 11:40:47 +0700

    enqueue js file

--- functions.php
+++ functions.php
@@ -6,8 +6,8 @@
 
 function starter_get_parent_style() {
 
-    wp_enqueue_style( 'divi-styles', get_theme_file_uri( '/style.css' ), array());
-
+    wp_enqueue_style( 'divi-styles', get_theme_file_uri( '/style.css' ));
+    wp_enqueue_script( 'starter-script', get_theme_file_uri() .'/js/starter.js', array('jquery'), true);
 }
 add_action( 'wp_enqueue_scripts', 'starter_get_parent_style' );
 
